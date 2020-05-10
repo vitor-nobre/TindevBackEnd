bdee2e42283e48757630ea62d183878d408a576d
vitor <vitorn2009@hotmail.com>
2020-05-10 01:58:57 -0300

    Criação de um método no controller para sinalizar quando da match.

--- src/server.js
+++ src/server.js
@@ -3,15 +3,34 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+io.on('connection', socket =>{
+    const { user } = socket.handshake.query;
+    
+    connectedUsers[user] = socket.id;
+});
+
+
 mongoose.connect('mongodb+srv://erbon:erbon@tindev-db-7mtkr.mongodb.net/dbtindev?retryWrites=true&w=majority',{
     useUnifiedTopology: true,
     useNewUrlParser: true
 });
 
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
 
-server.use(cors());
-server.use(express.json());
+    return next();
+})
+
+app.use(cors());
+app.use(express.json());
 var porta = process.env.PORT || 8080;
-server.use(routes);
-server.listen(porta); 
+app.use(routes);
+
+
+server.listen(3333); 
